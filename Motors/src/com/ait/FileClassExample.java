2750b678ca725ebe1c84e84e73b8d6e58528f1e0
Your nakul <nakulchaudhari12345@gmail.com>
2022-12-21 11:14:13 +0530

    motors

---
+++ Motors/src/com/ait/FileClassExample.java
@@ -0,0 +1,28 @@
+package com.ait;
+
+import java.io.File;
+
+public class FileClassExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String filepath=".src/com/ait/restaurant.txt";
+		String anotherfilepath=".src/com/ait/restaurant.txt";
+		File currentfile = new File (filepath);
+		File anotherfile = new File(anotherfilepath);
+		boolean available= currentfile.exists();
+		if(available)
+			System.out.println("the file"+filepath+"available");
+		else
+			System.out.println("the file"+filepath +"not available");
+		boolean isfile =currentfile.isFile();
+		if(isfile)
+			System.out.println("it is a file");
+		
+		isfile =anotherfile.isFile();
+		if(isfile)
+			System.out.println("it is a file");
+		
+	}
+
+}
